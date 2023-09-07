237481dbd0f6a4c897d770ddde2deeaba91fa10b
João Victor <jjviktorpe@hotmail.com>
2023-09-07 15:46:07 -0300

    Condição Simples

---
+++ condições.js
@@ -0,0 +1,6 @@
+var vel = 10
+console.log(`A velozidade do seu caro é ${vel}km/h`)
+if (vel > 60) { //Condição simples!
+    console.log('Voce ultrapasou a Velocidade Permitida! Multado')
+}
+console.log('Diriga sempre usando cinto de segurança vagabundo!')
