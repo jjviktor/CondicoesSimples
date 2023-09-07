d1b437d0d0a18376291d0df0c9de49426473ce3b
João Victor <jjviktorpe@hotmail.com>
2023-09-07 17:56:26 -0300

    Horas

---
+++ hora.js
@@ -0,0 +1,13 @@
+var agora = new Date()
+var hora = agora.getHours ()
+var minutos = agora.getMinutes ()
+console.log(`Agora são extamente ${hora} horas e ${minutos} Minutos`)
+if (hora < 12) {
+    console.log('Bom dia')
+}
+else if (hora <= 18) {
+    console.log('Boa tarde')
+}
+else {
+    console.log('boa noite')
+}
